84ca0a4150a6b1574f0740571dc054ad6e1003ad
Mac_honglam <honglam.1292@gmail.com>
2021-10-21 19:03:43 +0700

    create footer component

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,37 +1,24 @@
 import { useTranslation } from 'next-i18next';
-import Link from 'next/link';
+import {
+  FacebookOutlined,
+  YoutubeOutlined
+} from '@ant-design/icons';
 
-import { Footer } from './styled';
+import { Footer, LeftInfor, RightInfor } from './styled';
 
-const FooterComponent = () : JSX.Element => {
+const FooterComponent = (): JSX.Element => {
   const { t } = useTranslation('footer');
   return (
     <>
       <Footer>
-        <div className={`px-14 py-16 md:py-10 text-white`}>
-          <div className="wrapper">
-            <div className="navigations space-x-0 md:space-x-6 uppercase footer-text font-medium tracking-widest space-y-6 md:space-y-0">
-              {/* <Link href="/press">
-                <a>{t('press')}</a>
-              </Link> */}
-              <Link href="/contact-us">
-                <a>{t('contact')}123123</a>
-              </Link>
-              <Link href="/term-conditions">
-                <a>{t('t&c')}</a>
-              </Link>
-              <Link href="/privacy-policy">
-                <a>{t('privacyPolicy')}</a>
-              </Link>
-              <Link href="/news">
-                <a>{t('news')}</a>
-              </Link>
-              <Link href="/faqs">
-                <a>{t('faqs')}</a>
-              </Link>
-            </div>
-          </div>
-        </div>
+        <LeftInfor>
+          © 2021 Bản quyền thuộc về Saba Sports
+        </LeftInfor>
+        <RightInfor>
+          Theo dõi Saba Sports trên
+          <FacebookOutlined  style={{ fontSize: '24px', margin: '0px 10px'}}/>
+          <YoutubeOutlined style={{ fontSize: '24px'}}/>
+        </RightInfor>
       </Footer>
     </>
   );
--- components/page/Home.tsx
+++ components/page/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useTranslation } from 'next-i18next';
+import { HomePage } from './styled';
 
 export default function Home() {
   const { t } = useTranslation('home');
   return (
-    <div>
+    <HomePage>
       <h1>{t('text1')}</h1>
       <h2>{t("product.name")}</h2>
-    </div>
+    </HomePage>
   )
 }
