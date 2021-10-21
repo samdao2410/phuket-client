04ccb20c34bf65b3f5a897296a933302d5ff24bc
Collins Pham <collinspham@Collinss-MacBook-Pro.local>
2021-10-21 17:26:27 +0700

    add layout banner control

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,54 +1,85 @@
+import React from 'react';
 import { Header } from './styled';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import { Select } from 'antd';
+import { Select, Dropdown, Menu } from 'antd';
+import {
+  SearchOutlined
+} from '@ant-design/icons';
+
 
 const { Option } = Select;
 
-const HeaderComponent = () : JSX.Element => {
+const HeaderComponent = (): JSX.Element => {
   const { t } = useTranslation('header');
   const router = useRouter();
-  const { pathname, asPath } = router;
+  const [lang, setLang] = React.useState('en');
+  const [showLang, setShowLang] = React.useState(false);
+  const { pathname, asPath, locale } = router;
 
   const langs = {
-    en: `${t('language.en')} (US)`,
-    jp: `${t('language.jp')} (JP)`,
-    // nl: `${t('language.nl')} (NL)`,
-    // fr: `${t('language.fr')} (FR)`,
-    // de: `${t('language.de')} (DE)`
-    it: `${t('language.it')} (IT)`
-    // zh: `${t('language.zh')} (ZH)`,
-    // ko: `${t('language.ko')} (KO)`,
-    // ar: `${t('language.ar')} (AR)`,
-    // es: `${t('language.es')} (ES)`
+    en: `${t('language.en')}`,
+    jp: `${t('language.jp')}`,
+    it: `${t('language.it')}`
   };
 
-  const onChange = (val : string) => {
+  const onChange = (val: string) => {
     router.push(pathname, asPath, {
       locale: val
     });
   };
+  const onClickLang = ({ key }) => {
+   console.log(key);
+  };
+  const menu = (
+    <Menu onClick={onClickLang} className='language-select'>
+      {
+        Object.keys(langs).map(key => (
+          <Menu.Item key={key}>{langs[key]}</Menu.Item>
+
+        ))
+      }
+    </Menu>
+  );
   return (
     <>
-      <Header>
-        <Select
-          showSearch
-          style={{ width: 200 }}
-          placeholder="Select language"
-          optionFilterProp="children"
-          onChange={onChange}
-          filterOption={(input, option : any) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }>
-          {Object.keys(langs).map((item) => (
-            <Option key={item} value={item}>
-              {langs[item]}
-            </Option>
-          ))}
-        </Select>
+      <Header className='clearfix'>
+        <div className='container'>
+          <div className='header-controls'>
+            <a href='#' className='banner'><img /><img src='/images/header/Desktop_320x70.png' /></a>
+            <p className='logo'><img src='/images/header/sabasports.svg' /></p>
+            <div className="header-auth-provider">
+              <Dropdown trigger={['click']} overlay={menu} className='languages-box'>
+                <p className='language-active'><span><img src={`/images/header/icon-${locale}.svg`} /></span>{langs[lang]}</p>
+              </Dropdown>
+              <div className='search'>
+                <input />
+                <SearchOutlined />
+              </div>
+              <p>Đăng nhập</p>
+            </div>
+          </div>
+
+        </div>
+
       </Header>
     </>
   );
 };
 
 export default HeaderComponent;
+{/* <Select
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Select language"
+            optionFilterProp="children"
+            onChange={onChange}
+            filterOption={(input, option: any) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }>
+            {Object.keys(langs).map((item) => (
+              <Option key={item} value={item}>
+                {langs[item]}
+              </Option>
+            ))}
+          </Select> */}
