0d254fd030be1f4e1c5dfd5fac7794f221264248
Collins Pham <collinspham@Collinss-MacBook-Pro.local>
2021-10-25 17:10:14 +0700

    add usePost

--- components/page/football/index.tsx
+++ components/page/football/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import RightDetail from './RightDetail';
 import LeftDetail from './LeftDetail';
+import { useRouter } from 'next/router';
+import { ID_CATEGORIES } from 'constant/cateogries';
+import {useGetPosts} from 'utils/useGetPosts';
+
 
 interface FootBallPageType {
   typePage?: string | undefined | any
 }
 
 function FootBallPage({ typePage }: FootBallPageType) {
+  const router : any = useRouter();
+  const locale = router.locale;
+  let id;
+  if (!typePage) {
+    id = ID_CATEGORIES[locale]?.football
+  }
+  const { posts } = useGetPosts(id);
+  console.log({id,posts})
   return (
     <div className="container pt-6 pb-10">
       <div className="grid grid-cols-12 gap-4">
--- utils/useGetPosts.ts
+++ utils/useGetPosts.ts
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Api } from './api';
 
+export const useGetPosts = (id: string | number) => {
+  const [posts, setPosts] = React.useState([]);
 
-
-export const useGetPosts = (id: string) => {
-
-}
-
-
+  React.useEffect(() => {
+    (async () => {
+      const result = await Api.get(`/posts?categories=${id}`);
+      const posts = result.data;
+      setPosts(posts);
+    })();
+  }, []);
+  return {
+    posts
+  };
+};
